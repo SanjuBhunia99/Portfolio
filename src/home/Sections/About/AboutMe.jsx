be604b0127ecf0f03948ad4ae66442ca91d1ec11
SanjuBhunia99 <sanjubhunia93@gmail.com>
2026-01-08 23:26:00 +0530

    done

--- src/home/Sections/About/AboutMe.jsx
+++ src/home/Sections/About/AboutMe.jsx
@@ -23,7 +23,7 @@ const AboutMe = () => {
       </h1>
 
       <p className="mt-4 text-gray-200 text-sm md:text-base max-w-xl">
-        Software Engineer <span className="font-semibold">India</span>
+        Software Engineer from <span className="font-semibold">India</span>
       </p>
 
       <div className="flex gap-5 mt-6">
