2af637e1c1944c24b79a180150bfc05a507b7c2a
Sanju Bhunia <sanjubhunia93@gmail.com>
2026-01-12 12:59:53 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./home/home";
 import Footer from "./components/Footer";
+import PointerFollower from "./components/PointerFollow";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/footer" element={<Footer />} />
+        <Route path="/PointerFollow" element={<PointerFollower />} />
       </Routes>
     </>
   );
