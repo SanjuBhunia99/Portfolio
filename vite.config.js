419e7068fa345c3658a202d29964193018b85ac1
SanjuBhunia99 <sanjubhunia93@gmail.com>
2026-02-22 09:44:34 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({jsxRuntime: "automatic"}),
-  tailwindcss(),
-  ],
-   base:"https://SanjuBhunia99.github.io/Portfolio",
-  
-   build: {
+  plugins: [react({ jsxRuntime: "automatic" }), tailwindcss()],
+  base: "https://sanjubhunia99.github.io/Portfolio/",
+
+  build: {
     target: "esnext",
     minify: "terser",
     terserOptions: {
