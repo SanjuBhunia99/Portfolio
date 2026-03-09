8812f9fb214910f899b155de5304e23235ae9aa1
SanjuBhunia99 <sanjubhunia93@gmail.com>
2026-03-09 18:49:35 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react({ jsxRuntime: "automatic" }), tailwindcss()],
-//   base: "https://sanjubhunia99.github.io/Portfolio/",
-
-//   build: {
-//     target: "esnext",
-//     minify: "terser",
-//     terserOptions: {
-//       compress: {
-//         drop_console: true,
-//         drop_debugger: true,
-//       },
-//     },
-//     cssCodeSplit: true,
-//     sourcemap: false,
-//     chunkSizeWarningLimit: 600,
-//     rollupOptions: {
-//       output: {
-//         manualChunks: {
-//           react: ["react", "react-dom"],
-//         },
-//       },
-//     },
-//   },
-
-//   optimizeDeps: {
-//     include: ["react", "react-dom"],
-//   },
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
