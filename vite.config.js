69daeb496638a48792efc9acb6801bc50e9be8cb
SanjuBhunia99 <sanjubhunia93@gmail.com>
2026-03-09 18:42:33 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react({ jsxRuntime: "automatic" }), tailwindcss()],
+//   base: "https://sanjubhunia99.github.io/Portfolio/",
+
+//   build: {
+//     target: "esnext",
+//     minify: "terser",
+//     terserOptions: {
+//       compress: {
+//         drop_console: true,
+//         drop_debugger: true,
+//       },
+//     },
+//     cssCodeSplit: true,
+//     sourcemap: false,
+//     chunkSizeWarningLimit: 600,
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           react: ["react", "react-dom"],
+//         },
+//       },
+//     },
+//   },
+
+//   optimizeDeps: {
+//     include: ["react", "react-dom"],
+//   },
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react({ jsxRuntime: "automatic" }), tailwindcss()],
-  base: "https://sanjubhunia99.github.io/Portfolio/",
+  base: "/Portfolio/",
 
   build: {
     target: "esnext",
@@ -20,8 +54,10 @@ export default defineConfig({
     chunkSizeWarningLimit: 600,
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
         },
       },
     },
@@ -35,5 +71,3 @@ export default defineConfig({
     open: true,
   },
 });
-
-
