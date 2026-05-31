c33e1e01614c655caa2a9851bacc2a77d7a59e79
Sanju Bhunia <sanjubhunia93@gmail.com>
2026-05-31 23:24:12 +0530

    footer all link fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,6 @@ const Footer = () => {
   return (
     <footer className="relative pt-3 bg-linear-to-tr from-[#280b31ea] via-[#3a1850e0] to-[#4e1e5be0] text-white mt-8 shadow-2xl overflow-x-hidden">
       <div className="max-w-7xl mx-auto px-4 pb-6 pt-8 flex flex-col md:flex-row md:justify-between items-center gap-6 md:gap-8">
-       
         <div className="order-1 md:order-2 w-full flex justify-center">
           <img
             src="/Portfolio/UI-Developer.gif"
@@ -15,7 +14,6 @@ const Footer = () => {
           />
         </div>
 
-        
         <div className="flex flex-col items-center order-2 md:order-3 w-full">
           <span className="text-base font-semibold mb-2 tracking-wide">
             Social Media
@@ -23,7 +21,7 @@ const Footer = () => {
 
           <div className="flex flex-wrap gap-4 mt-2 justify-center">
             <a
-              href="https://www.instagram.com/thesanju_bhunia99/"
+              href="https://www.instagram.com/thesanjubhunia99/"
               target="_blank"
               rel="noopener noreferrer"
               className="group"
@@ -34,7 +32,7 @@ const Footer = () => {
             </a>
 
             <a
-              href="https://www.linkedin.com/in/sanju-bhunia99/"
+              href="https://www.linkedin.com/in/sanjubhunia99/"
               target="_blank"
               rel="noopener noreferrer"
               className="group"
@@ -45,7 +43,7 @@ const Footer = () => {
             </a>
 
             <a
-              href="https://github.com/SanjuBhunia99"
+              href="https://github.com/sanjubhunia99/"
               target="_blank"
               rel="noopener noreferrer"
               className="group"
@@ -79,7 +77,6 @@ const Footer = () => {
           </div>
         </div>
 
-        
         <div className="flex flex-col items-center md:items-start gap-2 order-3 md:order-1 w-full">
           <span className="text-base sm:text-lg font-semibold tracking-wide text-center md:text-left">
             Made with 🧡 and React
